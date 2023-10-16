12932af8ddb4218644c29679246292c239c4df5b
Hildeberto Dantas <betodantas999@gmail.com>
2023-10-16 15:15:58 -0300

    preenchendo login

--- Preencher login/Exercico_Wagner.py
+++ Preencher login/Exercico_Wagner.py
@@ -1,9 +1,26 @@
+from time import sleep
 import pyautogui
-import time
 pyautogui.press('Win')
-time.sleep(2)
-pyautogui.write('Edge')
+sleep(2)
+pyautogui.write('Edge', interval=0.4)
 pyautogui.press('enter')
-time.sleep(2)
+sleep(2)
+
+link_login = "http://127.0.0.1:5500/index.html"
+pyautogui.write(link_login, interval=0.2)
+sleep(1)
+pyautogui.press("enter")
+sleep(1)
+pyautogui.press("tab")
+pyautogui.write("admin", interval=0.2)
+sleep(1)
+pyautogui.press("tab")
+pyautogui.write("admin", interval=0.2)
+sleep(1)
+pyautogui.press("tab")
+sleep(1)
+pyautogui.press("enter")
+sleep(2)
+pyautogui.press("enter")
 
 
