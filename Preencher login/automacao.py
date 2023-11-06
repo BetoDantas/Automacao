c51b92f32ed09bf0d4087fea203505b9ea4b5d1c
Hildeberto Dantas <betodantas999@gmail.com>
2023-11-05 22:58:36 -0300

    Update automacao.py

--- Preencher login/automacao.py
+++ Preencher login/automacao.py
@@ -1,7 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+
 nav = webdriver.Chrome()
 
-nav.get("https://www.dio.me/")
-#nav.find_element_by_xpath()
+# Acessando a página
+nav.get("https://benchpromos.com")
+
+# Esperando o carregamento da página
 time.sleep(6)
+
+# Localizando e clicando no botão "Entrar" utilizando XPath
+elemento_entrar = WebDriverWait(nav, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//a[@href="/sign-in" and contains(text(), "Entrar")]'))
+)
+elemento_entrar.click()
+
+# Esperando um tempo para a página de login ser carregada
+time.sleep(6)
+
+# Localizando e preenchendo o campo de email
+campo_email = WebDriverWait(nav, 10).until(
+    EC.visibility_of_element_located((By.NAME, 'email'))  # Altere 'email' para o nome real do campo de email
+)
+campo_email.send_keys('testefaculdade804@gmail.com')
+time.sleep(2)
+
+# Localizando e preenchendo o campo de senha
+campo_senha = WebDriverWait(nav, 10).until(
+    EC.visibility_of_element_located((By.NAME, 'password'))  # Altere 'password' para o nome real do campo de senha
+)
+campo_senha.send_keys('1234567#')
+time.sleep(2)
+# Enviando o formulário de login (clicar no botão "Entrar")
+campo_senha.submit()
+
+# Esperando um tempo para observar o comportamento após o envio dos dados
+time.sleep(6)
+
+# Fechando o navegador
+nav.quit()
